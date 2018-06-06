d49da3df03a5883f03bd829f2381fb2cdac6d0ad
shtahvildary <shtahvildary@gmail.com>
2018-06-06 10:19:50 +0430

    some tests

--- src/App.js
+++ src/App.js
@@ -2,7 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function WallNode(){
+  return(<div><h1>hello...WallNode</h1></div>)    
+  }
+  
+  function Rackroom(){
+    return(<div><h1>hello...Rackroom</h1></div>)    
+    }
+    function Switchs(){
+      return(<div><h1>hello...Switchs</h1></div>)    
+      }
+
 class App extends Component {
+
+
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    // const response = await fetch('http://localhost:5000/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+
+
+
   render() {
     return (
       <div className="App">
@@ -13,9 +49,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p className="App-intro">{this.state.response}</p>
+
+
+        <WallNode/>
+        <Rackroom/>
+        <Switchs/>
       </div>
     );
   }
 }
+
 
 export default App;
