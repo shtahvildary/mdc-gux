66b2f85f898091e5b98bb871c52e293778383d74
shtahvildary <shtahvildary@gmail.com>
2018-06-07 15:23:21 +0430

    working on login page

--- src/App.js
+++ src/App.js
@@ -1,65 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './App.css';
-
-function WallNode(){
-  return(<div><h1>hello...WallNode</h1></div>)    
-  }
-  
-  function Rackroom(){
-    return(<div><h1>hello...Rackroom</h1></div>)    
-    }
-    function Switchs(){
-      return(<div><h1>hello...Switchs</h1></div>)    
-      }
+import LoginScreen from './Loginscreen';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends Component {
-
-
-  state = {
-    response: ''
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
+    }
   }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    // const response = await fetch('http://localhost:5000/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-
-
-
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<LoginScreen appContext={this}/>);
+    this.setState({
+                  loginPage:loginPage
+                    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <p className="App-intro">{this.state.response}</p>
-
-
-        <WallNode/>
-        <Rackroom/>
-        <Switchs/>
+        {this.state.loginPage}
+        {this.state.uploadScreen}
       </div>
     );
   }
 }
-
 
 export default App;
