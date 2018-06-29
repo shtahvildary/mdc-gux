1ffbede38984ee934cb84e48c136f1409bc52ec7
shtahvildary <shtahvildary@gmail.com>
2018-06-29 23:57:19 +0430

    table is ok

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,184 +1,96 @@
-import React from 'react';
-import mapKeys from 'lodash';
+import React from "react";
+import _ from "lodash";
 
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import PropTypes from 'prop-types';
-
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import PropTypes from "prop-types";
 
 const styles = theme => ({
-    root: {
-      width: '100%',
-      marginTop: theme.spacing.unit * 3,
-      overflowX: 'auto',
-    },
-    table: {
-      minWidth: 700,
-    },
-  });
-let id = 0;
-function createData(name, cable, swName, swPort, vlan,systemType,description) {
-    id += 1;
-    return { id, name, cable, swName, swPort, vlan,systemType,description };
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 700
   }
-  const data = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0,34,'sfgh'),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3,56,'fghdfh'),
-    createData('Eclair', 262, 16.0, 24, 6.0,47,'hgjfgh'),
-    createData('Cupcake', 305, 3.7, 67, 4.3,989,'sfghdf'),
-    createData('Gingerbread', 356, 16.0, 49, 3.9,546,'gfjhfg'),
-  ];
+});
 
-class SimpleTable extends React.Component{
-  
-
-  constructor(props){
-    console.log("props: ",props);
+class SimpleTable extends React.Component {
+  constructor(props) {
     super(props);
-  this.state={
-    root:'',
-    table:'',
-    columns:props.columns,
-    data:props.data,
-    col:[],
-    row:[],
+    this.state = {
+      root: "",
+      table: "",
+      columns: [],
+      data: [],
+      col: [],
+      row: [],
+      columnComponents: [<TableCell>default</TableCell>]
+    };
   }
-//   var col=[];
-//   var row=[]
-//     this.state.data.map(d=>{
-//       console.log("d: ",d)
-//       mapKeys(d,(key,value)=>{
-//         this.setState(col.push({column:key}))
-//          this.setState(row.push({column:key,data:value}))
-
-          
-//       })
-//   }
-// );
-
-
+  componentWillMount() {
+    this.setState({ data: this.props.data, columns: this.props.columns });
   }
-  render(){
-   
-    // console.log("col: ",col)
-    var col=[];
-    var row=[];
-    console.log(this.state)
+  componentWillReceiveProps(newProps) {
+    var col = [];
+    var rows = [];
 
-      this.state.data.map(d=>{
-        console.log("d: ",d)
-        mapKeys(d,(key,value)=>{
-          col.push({column:key})
-           row.push({column:key,data:value})        
-        })
-    }
-  );
-  this.setState({col,row})
-
-    return(
-
-    <Paper className={this.state.root}>
-      <Table className={this.state.table}>
-        <TableHead>
-          <TableRow>
-            {this.state.col.map(c=>{
-              console.log("c: ",c)
-              return(
-            <TableCell>{c}</TableCell>
-
-              )
-            })}
-              {/* bayad ba key ha por shavad */}
-            {/* <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell numeric>شماره کابل</TableCell>
-            <TableCell numeric>نام سوییچ</TableCell>
-            <TableCell numeric>شماره پورت</TableCell>
-            <TableCell numeric>vlan</TableCell>
-            <TableCell numeric>systemType</TableCell>
-            <TableCell numeric>توضیحات</TableCell> */}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {data.map(n => {
-              return (
-                <TableRow key={n.id}>
-                  <TableCell component="th" scope="row">{n.name}</TableCell>
-                  <TableCell numeric>{n.cable}</TableCell>
-                  <TableCell numeric>{n.swName}</TableCell>
-                  <TableCell numeric>{n.swPort}</TableCell>
-                  <TableCell numeric>{n.vlan}</TableCell>
-                  <TableCell numeric>{n.systemType}</TableCell>
-                  <TableCell numeric>{n.description}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-          
+    _.mapKeys(this.state.columns, (value, key) => {
+      col.push({ column: key, title: value });
+    });
+    //rows=>>
+    newProps.data.map((d, i) => {
+      rows.push(col);
+      _.mapKeys(d, (value, key) => {
+        var j = rows[i].findIndex(ci => ci.column === key);
+        if (j !== -1) rows[i][j].data = value;
+      });
+      col.map(c => {
+        if (rows[i].findIndex(ri => ri.column === c.column) === -1)
+          rows[i].push({ column: c.column, data: "-" });
+      });
+    });
+    this.setState({ data: newProps.data, col, rows }, () => {
+      this.setColumns();
+      this.setRows();
+    });
+  }
+  setColumns() {
+    var columnComponents = this.state.col.map(c => {
+      return <TableCell>{c.title}</TableCell>;
+    });
+    this.setState({ columnComponents }, () => {});
+  }
+  setRows() {
+    var rowsComponets = this.state.rows.map(r => {
+      var thisRow = r.map(cell => {
+        return <TableCell>{cell.data}</TableCell>;
+      });
+      return <TableRow>{thisRow}</TableRow>;
+    });
+    this.setState({ rowsComponets }, () => {});
+  }
+  render() {
+    return (
+      <Paper className={this.state.root}>
+        <Table className={this.state.table}>
+          <TableHead>
+            <TableRow>{this.state.columnComponents}</TableRow>
+          </TableHead>
+          <TableBody>{this.state.rowsComponets}</TableBody>
         </Table>
       </Paper>
-      
-          )
-    
+    );
   }
-
 }
 
-// export default SimpleTable;
-  // function SimpleTable(props) {
-  
-  //   const { classes } = props;
-  //   console.log(props)
-  //   props.data.map(d=>{
-  //       mapKeys(d,(key,value)=>{
-            
-  //       })
-  //   });
-  //   return (
-  //     <Paper className={classes.root}>
-  //     <Table className={classes.table}>
-  //       <TableHead>
-  //         <TableRow>
-  //             {/* bayad ba key ha por shavad */}
-  //           <TableCell>Dessert (100g serving)</TableCell>
-  //           <TableCell numeric>شماره کابل</TableCell>
-  //           <TableCell numeric>نام سوییچ</TableCell>
-  //           <TableCell numeric>شماره پورت</TableCell>
-  //           <TableCell numeric>vlan</TableCell>
-  //           <TableCell numeric>systemType</TableCell>
-  //           <TableCell numeric>توضیحات</TableCell>
-  //         </TableRow>
-  //       </TableHead>
-  //       <TableBody>
-          
-  //             {/* bayad ba value ha por shavad */}
-  //           {data.map(n => {
-  //             return (
-  //               <TableRow key={n.id}>
-  //                 <TableCell component="th" scope="row">{n.name}</TableCell>
-  //                 <TableCell numeric>{n.cable}</TableCell>
-  //                 <TableCell numeric>{n.swName}</TableCell>
-  //                 <TableCell numeric>{n.swPort}</TableCell>
-  //                 <TableCell numeric>{n.vlan}</TableCell>
-  //                 <TableCell numeric>{n.systemType}</TableCell>
-  //                 <TableCell numeric>{n.description}</TableCell>
-  //               </TableRow>
-  //             );
-  //           })}
-  //         </TableBody>
-          
-  //       </Table>
-  //     </Paper>
-      
-  //   );
-   
-  // }
-  
-  SimpleTable.propTypes = {
-    data: PropTypes.object.isRequired,
-  };
-  export default withStyles(styles)(SimpleTable);
+SimpleTable.propTypes = {
+  data: PropTypes.object.isRequired
+};
+export default withStyles(styles)(SimpleTable);
--- src/netNodes.js
+++ src/netNodes.js
@@ -1,72 +1,48 @@
-import React,{Component} from 'react';
-import axios from 'axios';
-import SimpleTable from './components/Table'
+import React, { Component } from "react";
+import axios from "axios";
+import SimpleTable from "./components/Table";
 
 class netNodeTable extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      response:'',
-      columns:[],
-      
-    }
+    this.state = {
+      response: [],
+      columns: []
+    };
   }
   componentWillMount() {
     this.callApi()
-      .then(res => {console.log(res)
-        this.setState({ response: res.data.netNodes })})
+      .then(res => {
+        this.setState({ response: res.data.netNodes });
+      })
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await axios({method:'post',url:global.serverAddress+'/netnodes/all',headers:{"x-access-token":localStorage.getItem('token')}});
+    const response = await axios({
+      method: "post",
+      url: global.serverAddress + "/netnodes/all",
+      headers: { "x-access-token": localStorage.getItem("token") }
+    });
     const body = await response;
-    // const body = await response.json();
-
     if (response.status !== 200) throw Error(body.message);
-    // console.log(body)
 
     return body;
   };
-  render(){
-    // console.log(".....",this.response)
-
-
-// axios({method:'post',url:global.serverAddress+'/netnodes/all',headers:{"x-access-token":localStorage.getItem('token')}})
-// .then(function(response){
-  // if(response.status===200){
-    return(
-  //   <SimpleTable columns={[{location:"مکان",switchPort:"شماره پورت سوییچ",switchId:"سوییچ",cabelNumber:"شماره کابل",patchPanelPort:"شماره patch panel"}]} data={[
-  //     {
-  //         "location": "5b261ae413c47e20fbb61d59",
-  //         "switchPort": "32",
-  //         "switchId": "5afe796f44ac79270a15e45a",
-  //         "cabelNumber": "32",
-  //         "patchPanelPort": "R5P612",
-         
-  //     },{
-  //       "location": "5b261ae413c47e20fbb61d59",
-  //       "switchPort": "32",
-  //       "switchId": "5afe796f44ac79270a15e45a",
-  //       "cabelNumber": "32",
-  //       "patchPanelPort": "R5P612",
-  //       "updatedAt": "2018-06-17T12:58:01.583Z",
-  //   }
-  // ]} parentContent={this} appContext={this.props.appContext}/>
-  
-  <SimpleTable  columns={[{location:"مکان",switchPort:"شماره پورت سوییچ",switchId:"سوییچ",cabelNumber:"شماره کابل",patchPanelPort:"شماره patch panel"}]} data={this.state.response}/>
-  // <SimpleTable  parentContent={this} appContext={this.props.appContext}/>
-)
-
+  render() {
+    return (
+      <SimpleTable
+        columns={{
+          location: "مکان",
+          switchPort: "شماره پورت سوییچ",
+          switchId: "سوییچ",
+          cabelNumber: "شماره کابل",
+          patchPanelPort: "شماره patch panel"
+        }}
+        data={this.state.response}
+      />
+    );
   }
-  // else{
-  //   return null;
-  // }
-// }).catch(e=>{
-//   console.log(e)
-//   return null;
-// })
-// }
 }
 
 export default netNodeTable;
