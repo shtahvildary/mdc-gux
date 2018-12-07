c6eba85a574be65b4416645842472e67822cfb58
sh <shtahvildary@yahoo.com>
2018-12-07 22:52:16 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter,Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter,Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import axios from 'axios';
-import Dashboard from './Dashboard';
-import netNodes from './NetNodes';
-import locations from './Locations';
-import vlans from './Vlans';
-import Devices from './Devices';
-import NewNetNode from './NewNetNode';
-import NewSwitch from './NewSwitch';
-import NewLocation from './NewLocation';
-import NewDepartment from './NewDepartment';
-import NewUser from './NewUser';
+// import Dashboard from './Dashboard';
+// import netNodes from './NetNodes';
+// import locations from './Locations';
+// import vlans from './Vlans';
+// import Devices from './Devices';
+// import NewNetNode from './NewNetNode';
+// import NewSwitch from './NewSwitch';
+// import NewLocation from './NewLocation';
+// import NewDepartment from './NewDepartment';
+// import NewUser from './NewUser';
 // import Register from './Register';
-import Switches from './Switches';
-import NewVlan from './NewVlan';
-import NewDevice from './NewDevice';
-import NewDeviceType from './NewDeviceType';
+// import Switches from './Switches';
+// import NewVlan from './NewVlan';
+// import NewDevice from './NewDevice';
+// import NewDeviceType from './NewDeviceType';
 
-global.serverAddress = "http://172.16.17.195:5000/api";
-// global.serverAddress = "http://localhost:5000/api";
+// global.serverAddress = "http://172.16.17.195:5000/api";
+global.serverAddress = "http://localhost:5000/api";
 // global.streamServerAddress = "http://localhost:8001/api";
+
 
  var callApi=async () => {
     const response = await axios({ method: 'post', url: global.serverAddress + '/users/me', headers: { "x-access-token": localStorage.getItem('token') } });
