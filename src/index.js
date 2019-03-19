fd5fa784326b5e14d4ff7792c8ad731753e46fdc
shtahvildary <shtahvildary@gmail.com>
2019-03-19 09:47:58 +0330

    add new ip

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import axios from 'axios';
 
 
 // global.serverAddress = "http://172.16.17.191:5000/api";
+global.serverAddress = "http://172.16.16.163:5000/api";
 // global.serverAddress = "http://172.16.17.195:5000/api";
 // global.serverAddress = "http://192.168.1.2:5000/api";
-global.serverAddress = "http://localhost:5000/api";
+// global.serverAddress = "http://localhost:5000/api";
 // global.streamServerAddress = "http://localhost:8001/api";
 
 
