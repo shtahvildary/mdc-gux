10481bf153738523aeeb30d4545193fa74e9d6bf
sh <shtahvildary@yahoo.com>
2019-04-13 22:57:22 +0430

    Update ExpantionPanel.js

--- src/components/ExpantionPanel.js
+++ src/components/ExpantionPanel.js
@@ -50,12 +50,22 @@ class SimpleExpansionPanel extends React.Component {
 
   componentWillReceiveProps(newProps) {
     const { classes } = newProps;
-    console.log(newProps)
+    // console.log(newProps)
     if (newProps.items===this.state.items) return;
     // var items = this.state.items
     var items = []
+    var keys,details
     newProps.items.map(i => {
-      console.log(i.details)
+      // console.log(i.details)
+      
+       keys=Object.keys(i.details)
+       details=""
+       keys.map(k=>{
+         if(k!="_id")         
+        details+=k+": "+i.details[k]+"\r\n"
+        // details+="\r\n"+k+": "+i.details[k]+"\r\n"
+        // details+=k+": "+i.details[k]
+      })
       items.push(
         <ExpansionPanel>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
@@ -63,16 +73,18 @@ class SimpleExpansionPanel extends React.Component {
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
             <Typography>
-              {JSON.stringify(i.details)}
+              {details}
             </Typography>
           </ExpansionPanelDetails>
         </ExpansionPanel>)
     })
-    this.setState({ items,dataLength:newProps.dataLength }, () => { console.log(items) })
+    this.setState({ items,dataLength:newProps.dataLength }, () => { 
+      // console.log(items) 
+    })
   }
   render() {
     const { classes } = this.props;
-    console.log("props: ", this.state.items)
+    // console.log("props: ", this.state.items)
 
     return (
 
