3706fad4c3cfb73900790cd0fd171c74937e8841
sh <shtahvildary@yahoo.com>
2019-05-23 09:47:39 +0430

    new front

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/*',(req,res)=>{
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/precache-manifest.e3f3f7120e364a8608e32819f95c4da8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a56d1b96b3c2a389e68fcca31b5d98e",
+    "url": "/static/media/google_datacenter-wallpaper-960x600.7a56d1b9.jpg"
+  },
+  {
+    "revision": "f4e793d951f2e637576c567851092f2f",
+    "url": "/static/media/IMG_302122.f4e793d9.jpg"
+  },
+  {
+    "revision": "e8ae5adc730a81cc92954fd1986f3b0c",
+    "url": "/static/media/BBadr.e8ae5adc.eot"
+  },
+  {
+    "revision": "e3429be274c27dda9a82549ddbf93d74",
+    "url": "/static/media/BBadr.e3429be2.ttf"
+  },
+  {
+    "revision": "ae12f7af23a34425d72afa5c2f74c019",
+    "url": "/static/media/BBadr.ae12f7af.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "baefbd5e653531075ca2",
+    "url": "/static/js/main.baefbd5e.chunk.js"
+  },
+  {
+    "revision": "3aa2bb1f6612c2f6bfaf",
+    "url": "/static/js/1.3aa2bb1f.chunk.js"
+  },
+  {
+    "revision": "baefbd5e653531075ca2",
+    "url": "/static/css/main.334e0139.chunk.css"
+  },
+  {
+    "revision": "ccf704401192ed5c876005e726aa6486",
+    "url": "/index.html"
+  }
+];
