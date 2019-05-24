02804b1551365eb83b3da206b7a19c1887450da9
sh <shtahvildary@yahoo.com>
2019-05-24 18:53:38 +0430

    new env variables

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ import axios from 'axios';
 
 
 
-global.serverAddress = "http://172.16.16.164:5000/api";
+// global.serverAddress = "http://172.16.16.164:5000/api";
 // global.serverAddress = "http://172.16.17.195:5000/api";
 // global.serverAddress = "http://localhost:5000/api";
 // global.streamServerAddress = "http://localhost:8001/api";
+console.log("process.env",process.env)
+if(process.env.REACT_APP_PROJECT_MODE=="Home")
+  global.serverAddress = process.env.REACT_APP_URL_HOME;
+else if(process.env.REACT_APP_PROJECT_MODE=="Office")
+global.serverAddress = process.env.REACT_APP_URL_OFFICE;
+else if(process.env.REACT_APP_PROJECT_MODE=="Server")
+global.serverAddress = process.env.REACT_APP_URL_SERVER;
+global.serverAddress+="/api"
+
+
+
 
 
  var callApi=async () => {
